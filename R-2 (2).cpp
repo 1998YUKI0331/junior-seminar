971112e8277d9dd07f081ee5b5ce207013e8c1af
YUKI KIM <67870795+1998YUKI0331@users.noreply.github.com>
2021-09-04 15:33:04 +0900

    교안에 답 잘못나와 있는 듯 ..

---
+++ R-2 (2).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	int n, A[100000], B[100000];
+	int max_A = 0;
+	
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> A[i];
+		max_A = max(max_A, A[i]); //AÀÇ ÃÖ´ñ°ª
+	}
+
+	for (int i = 0; i < n - 1; i++) {
+		int temp_A = A[i]; int temp_B = max_A;
+		for (int j = i + 1; j < n; j++) {
+			if (temp_A < A[j] && temp_B >= A[j]) {		
+				temp_B = A[j];
+				B[i] = j;
+			}
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < n - 1; i++) cout << B[i] << " ";
+
+	return 0;
+}
