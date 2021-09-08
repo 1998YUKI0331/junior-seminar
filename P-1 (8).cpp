40a5fd8ebb47c5abae14388f7f2b812ee5e06cf2
YUKI KIM <67870795+1998YUKI0331@users.noreply.github.com>
2021-09-08 15:36:43 +0900

    간단한 배열 만지는 문제

---
+++ P-1 (8).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	int cost; vector<int> arr(500000);
+	int result = 0; //연속된 최대 길이
+	cin >> cost;
+	
+	int idx = 0; //arr 배열의 길이
+	while (1) {
+		int temp = 0; cin >> temp;
+		if (temp == -1) break;
+		arr[idx++] = temp;
+	}
+
+	int start = 0; int sum = 0;
+	for (int end = 0; end < idx; end++) {
+		result = max(result, end - start);
+		sum += arr[end];
+		while (sum > cost) {
+			sum -= arr[start++];
+		}
+	}
+
+	cout << result;
+
+	return 0;
+}
